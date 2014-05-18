a8c05580ca964d666d2773075531081c8642a64e
Brandon Stirnaman <stirno@gmail.com>
2014-05-17 20:22:26 -0500

    Add PvcLess plugin

---
+++ src/PvcLess.cs
@@ -0,0 +1,26 @@
+﻿using PvcCore;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PvcPlugins
+{
+    public class PvcLess : PvcPlugin
+    {
+        public override IEnumerable<PvcStream> Execute(IEnumerable<PvcStream> inputStreams)
+        {
+            var lessEngine = new dotless.Core.LessEngine();
+            var resultStreams = new List<PvcStream>();
+
+            foreach (var inputStream in inputStreams)
+            {
+                var lessContent = inputStream.ToString();
+                var cssContent = lessEngine.TransformToCss(lessContent, "");
+
+                var resultStream = PvcUtil.StringToStream(cssContent, inputStream.StreamName);
+                resultStreams.Add(resultStream);
+            }
+
+            return resultStreams;
+        }
+    }
+}
